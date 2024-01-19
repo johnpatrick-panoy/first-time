c800e925c1196ec2d3bf3d70b071def140b892fe
johnpatrick <142279147+johnpatrick-panoy@users.noreply.github.com>
2024-01-19 09:56:33 +0800

    Add files via upload

---
+++ PANOY_JOHNPATRICK_prog_exer_1.py
@@ -0,0 +1,17 @@
+first = float( input("Enter first number: "))
+second = float( input("Enter second number: "))
+sum = first + second 
+print("Addition result:" + str(sum))
+sub = first - second 
+print("Subtraction result:"+ str(sub))
+mul = first * second
+print("Multiplication result:"+ str(mul))
+div = first / second
+print("Division result:"+ str(div))
+print("Equal result:" + str(first==second))
+print("Greater result:" + str(first>second))
+print("less  or equal result:" + str(first>=second))
+print("Bitwise AND result:" + str(0 & 0))
+print("Bitwise OR result:" + str(4|3))
+print("Logical AND result:" + str(first<4.6 and second<3.8))
+print("Logical OR result:" + str(first==4.6 or second==3.8))
